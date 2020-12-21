f45e9669766a7f80dcad7209c77852c24f218fe1
Scott Gloyna <scott.gloyna01@gmail.com>
2020-12-21 04:15:30 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,12 +1,16 @@
 # don't forget to add: require 'pry'
 require 'pry'
+player1="Rocky"
+player2="Bullwinkle"
 
 def start_game(player1, player2)
   binding.pry
-  "Hello #{player1} & #{playar2}"
+  puts "Hello #{player1} & #{playar2}"
 end
 
 def play_game(player2)
   binding.pry
-  "#{player1} is better than #{player2}"
+  puts "#{player1} is better than #{player2}"
 end
+
+start_game(player1, player2)
