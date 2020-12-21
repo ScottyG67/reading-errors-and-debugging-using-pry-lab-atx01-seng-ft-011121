a2f110ee30e250b4108eea2b37dc88885159a172
Scott Gloyna <scott.gloyna01@gmail.com>
2020-12-21 04:44:56 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,17 +1,15 @@
 # don't forget to add: require 'pry'
-require 'pry'
-#string = "urprise!"
+
 
 def snake_it_up(string)
-  #binding.pry
+
   if string[0] == "s"
-    #binding.pry
+
     10.times {string.prepend("s")}
   else
-   #binding.pry
+
     string
   end
   string
 end
 
-#snake_it_up(string)
