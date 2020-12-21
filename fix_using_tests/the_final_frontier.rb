f1706d2e595b734100e575366b90baf52a606f20
Scott Gloyna <scott.gloyna01@gmail.com>
2020-12-21 04:27:09 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -2,16 +2,19 @@
 require 'pry'
 
 def generate_star_date
-  
+  binding.pry
   (rand(100000) + 400000) / 10.0
 end
 
 
 def state_log(star_date)
+  binding.pry
   "Captain's Log, star date #{star_date}."
 end
 
 def engage
-  puts state_log(date)
   date = generate_star_date
+  puts state_log(date)
 end
+
+engage
